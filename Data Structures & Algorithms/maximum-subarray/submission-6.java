559585114e8ecccdc3891a6efb3fc72d61d11b59
Ati19 <57137664+Ati19@users.noreply.github.com>
2026-07-31 21:59:55 +0530

    Add: maximum-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-subarray/submission-6.java
@@ -0,0 +1,50 @@
+class Solution {
+    static Integer[][] memo;
+    public int maxSubArray(int[] nums) {
+        memo=new Integer[nums.length][2];
+        return dfs(nums,0,false);
+    }
+    private static int dfs(int[] nums,int i,boolean flag){
+        if(i==nums.length-1)
+        return flag?Math.max(0,nums[i]):nums[i];
+        int f=flag?1:0;
+        if(memo[i][f]!=null)return memo[i][f];
+        memo[i][f]=flag?    Math.max(0,nums[i]+dfs(nums,i+1,true)):
+        Math.max(dfs(nums,i+1,false),
+        nums[i]+dfs(nums,i+1,true));
+
+        return memo[i][f];
+    }
+    //     //recursion
+    //     return dfs(nums,0,false);
+    // }
+    // private static int dfs(int[] nums,int i,boolean flag){
+    //     if(i==nums.length-1)
+    //     return flag?Math.max(0,nums[i]):nums[i];
+    //     if(flag){
+    //         return Math.max(0,nums[i]+dfs(nums,i+1,true));
+    //     }
+    //     return Math.max(dfs(nums,i+1,false),nums[i]+dfs(nums,i+1,true));
+    
+    // }
+        //Using Kadane's algorithm
+        //if cur_sum is <0 no point keeping it
+        // int cur_sum=0,max_sum=nums[0];
+        // for(int num:nums){
+        //     if(cur_sum<0)
+        //     cur_sum=0;
+
+        //     cur_sum+=num;
+        //     max_sum=Math.max(cur_sum,max_sum);
+        // }
+        // return max_sum;
+        // if(nums.length==0) return 0;
+        // int current_sum=nums[0],max_sum=nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     current_sum=Math.max(current_sum+nums[i],nums[i]);
+        //     max_sum=Math.max(current_sum,max_sum);
+           
+        // }
+        // return max_sum;
+    // }
+}
