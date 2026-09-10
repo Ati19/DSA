ad4ada975f15e374c88f0d879982841fcd0b7323
Ati19 <57137664+Ati19@users.noreply.github.com>
2026-09-10 17:07:38 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.java
@@ -0,0 +1,42 @@
+class Solution {
+    static int[][] dp;
+    public boolean isSubsequence(String s, String t) {
+        int m=s.length();
+        int n=t.length();
+        // dp=new int[m+1][n+1];
+        // for(int[] t1:dp)
+        // Arrays.fill(t1,-1);
+        return lcs(s,t,m,n)==m;
+    }
+    public int lcs(String s,String t,int m, int n){
+        int[][] d=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0)
+                    d[i][j]=0;
+            }
+        }
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s.charAt(i-1)==t.charAt(j-1))
+                    d[i][j]=1+d[i-1][j-1];
+                else
+                 d[i][j]=Math.max(d[i-1][j],d[i][j-1]);
+            }
+        }
+        return d[m][n];
+
+    }
+
+    // public int lcsBottomUpDp(String s,String t,int m,int n){
+    //     if(m==0 || n==0)
+    //     return 0;
+    //     if(dp[m][n]!=-1)
+    //     return dp[m][n];
+    //     if(s.charAt(m-1)==t.charAt(n-1))
+    //     dp[m][n]= 1+ lcs(s,t,m-1,n-1);
+    //     else
+    //     dp[m][n]= Math.max(lcs(s,t,m-1,n),lcs(s,t,m,n-1));
+    //     return dp[m][n];
+    // }
+}
